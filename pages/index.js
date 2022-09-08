f1c8d30d44257032f794639c363c2cf8a4c16a9e
web3Codess <vercelploy@gmail.com>
2022-09-08 07:26:43 +0300

    .

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,15 @@ const index = () => {
 
  return (
    
-<div>
+<>
 
-<h1 className=' flex justify-center font-bold font-poppins'>
+<h1 className=' flex  font-bold font-poppins ml-2 mr-2'>
    HELPING COMPANIES SUCCEED IN WEB3 WORLD
 </h1>
 <p className="flex justify-center my-2 font-semibold font-poppins">
 CLICK TO DM US FOR A SERVICE 👇
 </p>
+
 
                <div className='flex justify-center'>
                <button type='button' className=" bg-black-600 px-7 py-2.5 text-lg md:text-sm rounded-md shadow-md shadow-blue-100 active:scale-90 hover:translate-y-2 transition-all">
@@ -24,8 +25,9 @@ CLICK TO DM US FOR A SERVICE 👇
                 </div>
 
 
+</>
 
-</div>
+
  )
   
 }
