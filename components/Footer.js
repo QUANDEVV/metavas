02c1ca29e5b9a907f251add34690da805c869270
web3Codess <vercelploy@gmail.com>
2022-09-08 11:51:51 +0300

    ..

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div  className=" w-full h-10 bg-white border-t-2  fixed left-0 bottom-0 flex justify-center items-center">
+    <div  className=" w-full h-10 bg-white border-t-2  fixed left-0 bottom-0 flex justify-center items-center bg">
     <Link href={`https://t.me/metavass`}>
     Click for a service 📥📩✉️
                </Link>
