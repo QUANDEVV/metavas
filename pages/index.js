4441b2c9873e0486d8c1fb70f015d136eac08fe0
web3Codess <vercelploy@gmail.com>
2022-09-08 08:19:43 +0300

    ..

--- pages/index.js
+++ pages/index.js
@@ -14,23 +14,46 @@ const index = () => {
 </h1>
 <p className="flex justify-center my-2 font-semibold font-poppins text-[0.9rem]">
 CLICK TO DM US FOR A SERVICE 👇
-</p>
+</p>   
+
+
+
+
 
 
                <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4  ">
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300 font-semibold ">
+                <Link href={`https://t.me/metavass`}>NFT MINTING SITE 
+               </Link>
+                </button>
+                </div>
+
+               <div className='flex justify-center mt-8'>
+               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300 font-semibold ">
                 <Link href={`https://t.me/metavass`}>NFT MINTING SITE 
                 GENERATE 10K NFT COLLECTION AND SELL</Link>
                 </button>
                 </div>
 
+                <div className='flex justify-center mt-8'>
+               <button type='button' className=" bg-black-600 px-10  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300 font-semibold ">
+                <Link href={`https://t.me/metavass`}>NFT MARKET PLACE LIKE OPENSEA
+                </Link>
+                </button>
+                </div>
+
+
+               
 
                 <div className='flex justify-center mt-8'>
-               <button type='button' className=" bg-black-600 px-2  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4  ">
+               <button type='button' className=" bg-black-600 px-10  text-lg md:text-sm rounded-md shadow-xl  shadow-blue-300 active:scale-90 hover:translate-y-2 transition-all border-t-4 border-blue-300 font-semibold ">
                 <Link href={`https://t.me/metavass`}>NFT WEBSITE
                 </Link>
                 </button>
                 </div>
+
+
+
 
 
 </>
